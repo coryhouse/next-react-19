a046d96e20f31c72d2db7d41fc1bd55f224ce67a
Cory House <housecor@gmail.com>
2024-07-01 08:10:52 -0500

    add spinner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Suspense } from "react";
 import Todos from "./Todos";
+import { Spinner } from "@/components/Spinner";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Todos</h1>
-      <Suspense fallback={<p>Loading...</p>}>
+      <h1 className="text-xl font-bold">Todos</h1>
+      <Suspense fallback={<Spinner />}>
         <Todos />
       </Suspense>
     </main>
