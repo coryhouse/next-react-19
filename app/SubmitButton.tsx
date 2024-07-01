5de016b89f311412033a334a3c7c7d968d5414db
Cory House <housecor@gmail.com>
2024-06-30 19:54:17 -0500

    add css support

--- app/SubmitButton.tsx
+++ app/SubmitButton.tsx
@@ -4,16 +4,18 @@ import { useFormStatus } from "react-dom";
 type SubmitButtonProps = {
   label: string;
   loadingLabel: string;
+  className?: string;
 };
 
 export function SubmitButton({
   label,
   loadingLabel,
+  className = "",
 }: Readonly<SubmitButtonProps>) {
   const { pending } = useFormStatus();
 
   return (
-    <button type="submit" disabled={pending}>
+    <button className={className} type="submit" disabled={pending}>
       {pending ? loadingLabel : label}
     </button>
   );
