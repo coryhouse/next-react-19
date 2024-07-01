8ca8a3c48f5c1893154d32af04deed97a05945f7
Cory House <housecor@gmail.com>
2024-07-01 07:49:44 -0500

    fix form

--- app/AddTodoForm.tsx
+++ app/AddTodoForm.tsx
@@ -4,13 +4,19 @@ import { SubmitButton } from "./SubmitButton";
 import { addTodo } from "./actions";
 
 export function AddTodoForm() {
+  // TODO: Rename to useActionState when new version of react-dom is released
+  // Next.js docs still reference useFormState
   const [state, formAction] = useFormState(addTodo, { titleError: "" });
 
   return (
     <form action={formAction}>
       <input type="text" name="title" />
-      <span>{state?.titleError}</span>
       <SubmitButton label="Add" loadingLabel="Adding..." />
+      {state?.titleError && (
+        <p role="alert" className="text-red-500">
+          {state?.titleError}
+        </p>
+      )}
     </form>
   );
 }
