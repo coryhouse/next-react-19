f16960f5604df3e72600f01c419c54001e35ae7c
Cory House <housecor@gmail.com>
2024-07-04 21:06:06 -0500

    add error boundary

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-xl font-bold">Oops!</h1>
+      <p className="text-red-500">An error occurred.</p>
+    </main>
+  );
+}
