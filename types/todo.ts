bb6d81958e9dadbfc1284feb7eacc4fb8f63ac06
Cory House <housecor@gmail.com>
2024-07-04 20:45:30 -0500

    add bool

--- types/todo.ts
+++ types/todo.ts
@@ -4,6 +4,7 @@ export const todoSchema = z.object({
   id: z.number(),
   title: z.string(),
   completed: z.boolean(),
+  saving: z.boolean().optional(),
 });
 
 export type Todo = z.infer<typeof todoSchema>;
