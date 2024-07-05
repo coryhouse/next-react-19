abb4284c041d1213f96e4b497ce6878dac8fe424
Cory House <housecor@gmail.com>
2024-07-04 20:45:21 -0500

    Unify and optimistic load

--- app/Todos.tsx
+++ app/Todos.tsx
@@ -1,31 +1,72 @@
-import { todoSchema } from "@/types/todo";
-import { AddTodoForm } from "./AddTodoForm";
+"use client";
+import { emptyAddToDoFormState, Todo } from "@/types/todo";
 import { SubmitButton } from "../components/SubmitButton";
-import { deleteTodo } from "./actions";
+import { addTodo, deleteTodo } from "./actions";
+import { useOptimistic, useRef } from "react";
+import { useFormState } from "react-dom";
 
-export default async function Todos() {
-  // NOTE: The trailing slash is required for the fetch to work
-  const resp = await fetch("http://localhost:3001/todos/", {
-    cache: "no-store",
-    next: { tags: ["todos"] },
-  });
-  const todos = todoSchema.array().parse(await resp.json());
+type TodosProps = {
+  todos: Todo[];
+};
+
+export function Todos({ todos }: TodosProps) {
+  // Next.js docs still reference useFormState
+  // On RSC apps, makes forms interactive before JavaScript has executed on the client.
+  // When used without Server Components, is equivalent to component local state.
+  const [state, formAction] = useFormState(addTodo, emptyAddToDoFormState);
+  const formRef = useRef<HTMLFormElement>(null);
+  const [optimisticTodos, addOptimisticTodo] = useOptimistic(
+    todos,
+    (state, newTodo: string) => [
+      ...state,
+      {
+        completed: false,
+        id: state.length + 1,
+        title: newTodo,
+        saving: true, // bool that tracks if the todo is currently in the process of saving
+      },
+    ]
+  );
 
   return (
     <>
       <ul>
-        {todos.map((todo) => (
+        {optimisticTodos.map((todo) => (
           <li key={todo.id} className="flex">
             <form action={deleteTodo}>
               <input type="hidden" name="id" value={todo.id} />
               <SubmitButton label="Delete" loadingLabel="Deleting..." />
             </form>
             {todo.title}
+            {todo.saving && (
+              <span className="text-sm text-slate-400 ml-2">Saving...</span>
+            )}
           </li>
         ))}
       </ul>
-
-      <AddTodoForm />
+      <form
+        ref={formRef}
+        action={(payload) => {
+          addOptimisticTodo(payload.get("title") as string);
+          formRef.current?.reset(); // Clear form upon submit
+          formAction(payload);
+        }}
+        className="mt-4"
+      >
+        <input
+          type="text"
+          name="title"
+          className={`${
+            state.titleError ? "border-red-500" : "border-gray-500"
+          } border p-1`}
+        />
+        <SubmitButton label="Add" loadingLabel="Adding..." />
+        {state?.titleError && (
+          <p role="alert" className="text-red-500">
+            {state?.titleError}
+          </p>
+        )}
+      </form>
     </>
   );
 }
