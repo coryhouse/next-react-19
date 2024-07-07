0e144d182f446047cca89e7ccc2e3410f8b83fed
Cory House <housecor@gmail.com>
2024-07-07 12:56:18 -0500

    rename

--- app/AddTodoForm.tsx
+++ app/AddTodoForm.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/Button";
 import Input from "@/components/Input";
 import { useRef } from "react";
 import { useFormState } from "react-dom";
-import { addTodo } from "./actions";
+import { addTodo } from "./todoActions";
 import { emptyAddToDoFormState } from "@/types/todo";
 
 type AddTodoFormProps = {
