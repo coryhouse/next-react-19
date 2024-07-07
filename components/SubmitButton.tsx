db2f68653e313d13a2375ac5b6d0f4e59b2485fd
Cory House <housecor@gmail.com>
2024-07-07 15:46:05 -0500

    refactor

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -3,20 +3,20 @@ import { useFormStatus } from "react-dom";
 import { Button } from "./Button";
 
 type SubmitButtonProps = {
-  label: string;
+  children: string;
   loadingLabel: string;
   className?: string;
 };
 
 export function SubmitButton({
-  label,
+  children,
   loadingLabel,
 }: Readonly<SubmitButtonProps>) {
   const { pending } = useFormStatus();
 
   return (
     <Button isLoading={pending} loadingLabel={loadingLabel} type="submit">
-      {label}
+      {children}
     </Button>
   );
 }
