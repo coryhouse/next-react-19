117937711d2b7d15aa75da905c27f3b8299fff1e
Cory House <housecor@gmail.com>
2024-10-28 07:57:41 -0500

    refactor

--- app/components/LoadingIndicator.tsx
+++ app/components/LoadingIndicator.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const LoadingIndicator: React.FC = () => {
+export default function LoadingIndicator() {
   return (
-    <div className="fixed bottom-4 right-4 z-50">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    <div className="fixed bottom-4 right-4 z-50 bg-yellow-500 w-full font-bold text-center">
+      Pending...{" "}
     </div>
   );
-};
-
-export default LoadingIndicator;
+}
