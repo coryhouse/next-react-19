0676d93089ca07f35edf40bed1a9fc6d3b9cb25d
Cory House <housecor@gmail.com>
2024-11-03 08:02:41 -0600

    add

---
+++ app/contact-react-18/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <title>Contact Us</title>
+      <h1>Contact Us</h1>
+      {children}
+    </>
+  );
+}
