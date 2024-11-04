125ca8f6ed7eb265d5b95e20905c88ae7ee077ea
Cory House <housecor@gmail.com>
2024-11-04 10:09:21 -0600

    fix

--- app/error.tsx
+++ app/error.tsx
@@ -2,9 +2,9 @@
 
 export default function Error() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <>
       <h1>Oops!</h1>
       <p className="text-red-500">An error occurred.</p>
-    </main>
+    </>
   );
 }
