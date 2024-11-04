7ab83a6b1c35801858755cba9b09b6349b5db9bf
Cory House <housecor@gmail.com>
2024-11-03 20:13:23 -0600

    Enhance

--- components/Errors.tsx
+++ components/Errors.tsx
@@ -1,8 +1,19 @@
-export function Errors({ errors }: { errors: string[] }) {
+import { ContactSchemaErrorType } from "@/app/contact/contact-form-schema";
+
+type ErrorsProps = {
+  errors: ContactSchemaErrorType;
+};
+
+export function Errors({ errors }: ErrorsProps) {
   return (
     <ul>
-      {errors.map((error) => (
-        <li key={error} className="text-red-500">
+      {Object.entries(errors.fieldErrors).map(([key, value]) => (
+        <li key={key} className="text-red-500">
+          {value}
+        </li>
+      ))}
+      {errors.formErrors.map((error, index) => (
+        <li key={index} className="text-red-500">
           {error}
         </li>
       ))}
