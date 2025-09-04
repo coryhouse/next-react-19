27e4dca5ace60ca602c2d0a11e57d0702b7a832d
Cory House <housecor@gmail.com>
2025-09-04 16:51:34 -0500

    fix styles and button type

--- components/Button.tsx
+++ components/Button.tsx
@@ -31,8 +31,10 @@ export function Button({
 }: ButtonProps) {
   return (
     <button
-      className={cx(className, "border border-slate-400 p-1 rounded", {
-        "bg-blue-600 text-white": variant === "primary",
+      type="button"
+      className={cx(className, "p-1 rounded", {
+        "bg-blue-600 text-white border border-slate-400": variant === "primary",
+        "bg-slate-200 border border-slate-400": variant === "secondary",
         "bg-white border-none p-1 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100":
           variant === "icon",
         "bg-inherit border-none text-gray-400": variant === "text",
