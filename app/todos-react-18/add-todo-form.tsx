14cf9f01fbbf2cf3c2d6c380b8841fee097d6afc
Cory House <housecor@gmail.com>
2025-09-05 17:09:14 -0500

    fix

--- app/todos-react-18/add-todo-form.tsx
+++ app/todos-react-18/add-todo-form.tsx
@@ -1,58 +1,65 @@
 import { Button } from "@/components/Button";
 import Input from "@/components/Input";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { emptyAddToDoFormState, taskSchema } from "@/types/todo";
 
 type AddTodoFormProps = {
   addOptimisticTodo: (task: string) => void;
+  markOptimisticTodoComplete: (id: string, task: string) => void;
 };
 
-export function AddTodoForm({ addOptimisticTodo }: AddTodoFormProps) {
+export function AddTodoForm({
+  addOptimisticTodo,
+  markOptimisticTodoComplete,
+}: AddTodoFormProps) {
   const [formState, setFormState] = useState(emptyAddToDoFormState);
   const [isPending, setIsPending] = useState(false);
 
-  async function addTodo(formData: FormData) {
-    const task = formData.get("task");
-    if (!task) return { error: "Task is required" };
-    try {
-      setIsPending(true);
-      const resp = await fetch("http://localhost:3001/todos/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ task, done: false }),
-      });
-      await resp.json();
-      setFormState(emptyAddToDoFormState);
-      setIsPending(false);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (_error) {
-      setFormState({ ...formState, error: "Failed to add '" + task + "'." });
+  async function addTodo(event: React.FormEvent) {
+    event.preventDefault();
+    const { task } = formState;
+    if (task) {
+      // Only add optimistic todo if task is not empty. This avoids a flash of the optimistic todo when the user clicks "add" with an empty todo is added.
+      addOptimisticTodo(task);
+      setFormState(emptyAddToDoFormState); // Clear form immediately upon submit rather than waiting for the optimistic add to complete (at which point React would reset automatically)
+      try {
+        setIsPending(true);
+        const resp = await fetch("http://localhost:3001/todos", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ task, done: false }),
+        });
+        const savedTodo = await resp.json();
+        markOptimisticTodoComplete(savedTodo.id, task);
+      } catch (_error) {
+        setFormState({
+          ...formState,
+          error: "Failed to add '" + task + "'.",
+        });
+      } finally {
+        setIsPending(false);
+      }
     }
   }
 
-  const formRef = useRef<HTMLFormElement>(null);
-
   return (
-    <form
-      ref={formRef}
-      onSubmit={(event) => {
-        const formData = new FormData(event.currentTarget);
-        const task = taskSchema.parse("task");
-        if (task) {
-          // Only add optimistic todo if task is not empty. This avoids a flash of the optimistic todo when the user clicks "add" with an empty todo is added.
-          addOptimisticTodo(task);
-          formRef.current?.reset(); // Clear form immediately upon submit rather than waiting for the optimistic add to complete (at which point React would reset automatically)
-        }
-        addTodo(formData);
-      }}
-    >
+    <form onSubmit={addTodo}>
       <Input
         id="task"
         label="Task"
         type="text"
-        name="task"
+        value={formState.task}
+        onChange={(e) =>
+          setFormState({
+            task: e.target.value,
+            error: taskSchema.safeParse(e.target.value).success
+              ? ""
+              : "Task must be a string.",
+          })
+        }
+        disabled={isPending}
         error={isPending ? undefined : formState.error}
         afterSlot={
           <Button type="submit" className="ml-2">
