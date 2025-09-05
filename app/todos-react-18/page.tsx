6e00f10ba07851965f53c0d7302c217c089d2adb
Cory House <housecor@gmail.com>
2025-09-05 16:47:02 -0500

    add error handling

--- app/todos-react-18/page.tsx
+++ app/todos-react-18/page.tsx
@@ -8,26 +8,31 @@ import { Todo as TodoType } from "@/types/todo";
 export default function Home() {
   const [todos, setTodos] = useState<TodoType[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     async function fetchTodos() {
-      const resp = await fetch("http://localhost:3001/todos/");
-      const parsedResp = savedTodoSchema.array().parse(await resp.json());
-      setTodos(parsedResp);
-      setIsLoading(false);
+      try {
+        const resp = await fetch("http://localhost:3001/todos/");
+        const parsed = savedTodoSchema.array().parse(await resp.json());
+        setTodos(parsed);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchTodos();
   }, []);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
+  if (isLoading) return <Spinner />;
+  if (error) throw error;
 
   return (
     <>
       <title>Todos</title>
       <h1>Todos</h1>
-      <Todos todos={todos} />;
+      <Todos todos={todos} />
     </>
   );
 }
