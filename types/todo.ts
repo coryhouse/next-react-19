5772e3beac0684993c925411955885494ad9072c
Cory House <housecor@gmail.com>
2025-09-04 20:06:12 -0500

    add comment

--- types/todo.ts
+++ types/todo.ts
@@ -12,8 +12,9 @@ export const savedTodoSchema = z.object({
   id: z.coerce.string(),
   task: taskSchema,
   done: z.boolean(),
-  // Optional because the db doesn't contain this field.
-  status: z.literal("saved").optional(),
+  // Using this because toggling the task's status is only allowed when it's saved.
+  // Optional because the DB doesn't contain this field.
+  status: z.literal("saved").optional().default("saved"),
 });
 
 export type SavedTodo = z.infer<typeof savedTodoSchema>;
