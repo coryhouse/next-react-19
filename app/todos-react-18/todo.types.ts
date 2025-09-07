67479a1dcac1062bd022395c8e0eaf7dde95a69b
Cory House <housecor@gmail.com>
2025-09-06 19:38:11 -0500

    Streamline

--- app/todos-react-18/todo.types.ts
+++ app/todos-react-18/todo.types.ts
@@ -3,18 +3,12 @@ import { z } from "zod";
 export const taskSchema = z.string();
 
 export const unsavedTodoSchema = z.object({
-  status: z.literal("unsaved"),
   task: taskSchema,
   done: z.boolean(),
 });
 
-export const savedTodoSchema = z.object({
+export const savedTodoSchema = unsavedTodoSchema.extend({
   id: z.coerce.string(),
-  task: taskSchema,
-  done: z.boolean(),
-  // Using this because toggling the task's status is only allowed when it's saved.
-  // Optional because the DB doesn't contain this field.
-  status: z.literal("saved").optional().default("saved"),
 });
 
 export type SavedTodo = z.infer<typeof savedTodoSchema>;
